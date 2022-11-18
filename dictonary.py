b412238164802b434fae66306008a9284b6b42dc
sagargola <111504053+sagargola@users.noreply.github.com>
2022-11-18 13:22:51 -0800

    Add files via upload
    Dictionary to input from user and show the value corresponding to keys

---
+++ dictonary.py
@@ -0,0 +1,5 @@
+#create a dictonary of 5 years and take the input from the user and display the meaning of the word
+
+dic = {"sagar":"gola", "shubham": "agarwal", "saurabh": "singh", "shipra":"sharma", "naveen":"pal"}
+name = input("enter the name")
+print (name, "means", dic[name])
