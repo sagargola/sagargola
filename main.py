8c6b4a07cf8bc05ba46d3686e5967b7ff51e8e34
sagargola <111504053+sagargola@users.noreply.github.com>
2022-11-18 13:30:34 -0800

    Add files via upload
    Basics of Python

---
+++ main.py
@@ -0,0 +1,44 @@
+#comment
+""" multi
+line
+comment
+
+print("sagr is \n good boy \t")
+
+var1 = "hello world"
+var2 = 4
+var3 = 36.3
+print(type(var3))
+print(10 * "hello world\n")
+print("eneter num")
+a = input()
+print("you enter", int(a)+10)
+
+
+print("enter 2 numbers")
+num1 = input()
+num2 = input()
+print("sum is", int(num1)+int(num2))
+"""
+str ="mynameiss12r"
+print(str[::-1])
+print(str.endswith("2r"))
+print(str.capitalize())
+print(str.lower())
+print(str.upper())
+print(str.replace("is", "are"))
+print(str.center(100))
+
+grocery = ["ace" , "aa", "bb" , "cc" ]
+print(grocery[2])
+numbers = [1,12,4,5,3]
+numbers.sort()
+print(numbers)
+print(numbers[2:3])
+numbers.append(22)
+print(numbers)
+numbers.remove(12)
+print(numbers)
+numbers.insert(1,33)
+print(numbers)
+dic =
