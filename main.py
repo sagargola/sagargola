7aa08ec2d3ade2e12f4f8def9b6be1b854ca49df
sagargola <111504053+sagargola@users.noreply.github.com>
2023-04-03 12:13:34 -0700

    Add files via upload
    Guessing a secret number using python where you have 3 guesses and secret number is assumed to 10.

--- main.py
+++ main.py
@@ -1,44 +1,13 @@
-#comment
-""" multi
-line
-comment
-
-print("sagr is \n good boy \t")
-
-var1 = "hello world"
-var2 = 4
-var3 = 36.3
-print(type(var3))
-print(10 * "hello world\n")
-print("eneter num")
-a = input()
-print("you enter", int(a)+10)
+Secret_number = 10
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+  guess = int(input("guess the number"))
+  guess_count+= 1
+  if guess == Secret_number:
+    print("you won")
+    break
+else:
+    print("you failed")
 
 
-print("enter 2 numbers")
-num1 = input()
-num2 = input()
-print("sum is", int(num1)+int(num2))
-"""
-str ="mynameiss12r"
-print(str[::-1])
-print(str.endswith("2r"))
-print(str.capitalize())
-print(str.lower())
-print(str.upper())
-print(str.replace("is", "are"))
-print(str.center(100))
-
-grocery = ["ace" , "aa", "bb" , "cc" ]
-print(grocery[2])
-numbers = [1,12,4,5,3]
-numbers.sort()
-print(numbers)
-print(numbers[2:3])
-numbers.append(22)
-print(numbers)
-numbers.remove(12)
-print(numbers)
-numbers.insert(1,33)
-print(numbers)
-dic =
